872c3e90131ea2aefc72bce6f19d6ef9543974e1
lmingz <lmzohio@gmail.com>
2018-12-04 22:23:05 +0800

    almost
    cant parse MainActivity.EXTRA_MESSAGE

--- SampleProject/app/src/main/java/com/sampleproject/sampleproject/MainActivity.java
+++ SampleProject/app/src/main/java/com/sampleproject/sampleproject/MainActivity.java
@@ -1,7 +1,12 @@
 package com.sampleproject.sampleproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
